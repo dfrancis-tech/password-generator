d97413be84c2d3c6675d397bb2ca93056d8b7a26
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2020-12-18 16:41:11 -0500

    display password in browser

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,9 @@
 
 // Character generator
 var generatePassword = function(length) {
+  // Choose length of password
   var length = window.prompt("Enter the length of password.");
+  
   // Selecting characters
   var charset = "";
   var lowCase = "abcdefghijklmnopqrstuvwxyz";
@@ -10,53 +12,54 @@ var generatePassword = function(length) {
   var numerics = "0123456789";
   var specialCharacters = "!#$%&'()*+,-./:;<=>?@[]\^_`{|}~";
   
-  var lc = window.confirm("Include lowercase alphbets");
-  if (lc === true) {
+  var includeThis = window.confirm("Include lowercase alphbets");
+  if (includeThis === true) {
     charset += lowCase;
   }
-  var lc = window.confirm("Include uppercase alphbets");
-  if (lc === true) {
+  var includeThis = window.confirm("Include uppercase alphbets");
+  if (includeThis === true) {
     charset += upperCase;
   }
-  var lc = window.confirm("Include numbers");
-  if (lc === true) {
+  var includeThis = window.confirm("Include numbers");
+  if (includeThis === true) {
     charset += numerics;
   }
-  var lc = window.confirm("Include symbols");
-  if (lc === true) {
+  var includeThis = window.confirm("Include symbols");
+  if (includeThis === true) {
     charset += specialCharacters;
   }
   if (charset === "") {
    window.alert("Please select atleast one type of character for generating password");
  }
   var n = charset.length;
-      output = "";
+      var output = "";
   for (var i = 0; i < length; ++i) {
       output += charset.charAt(Math.floor(Math.random() * n));
   }
   console.log(output);
-  return output;
-
+  // Write generated password inside textarea with class=card-body
+  document.getElementById("password").innerHTML = output;
 };
+
 
 // Password generation starts with the click of button with id = Generate
 generate.addEventListener("click", generatePassword);
 
-// Write generated password inside textarea with class=card-body
 
-
+/*
 // Get references to the #generate element
-// var generateBtn = document.querySelector("#generate");
+ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-/*
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}*/
+}
 
 // Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
+*/
