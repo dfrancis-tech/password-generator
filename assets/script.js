3df59a4610606acb6a6bdd4e18c48b68748eb969
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2020-12-18 12:25:41 -0500

    generate random caharacters with prompts

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,49 @@
 // Assignment code here
 
+// Character generator
+var generatePassword = function(length) {
+  // Selecting characters
+  var charset = "";
+  var lowCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numerics = "0123456789";
+  var specialCharacters = "!#$%&'()*+,-./:;<=>?@[]\^_`{|}~";
+  
+  var lc = window.confirm("Include lowercase alphbets");
+  if (lc === true) {
+    charset += lowCase;
+  }
+  var lc = window.confirm("Include uppercase alphbets");
+  if (lc === true) {
+    charset += upperCase;
+  }
+  var lc = window.confirm("Include numbers");
+  if (lc === true) {
+    charset += numerics;
+  }
+  var lc = window.confirm("Include symbols");
+  if (lc === true) {
+    charset += specialCharacters;
+  }
+  if (charset === "") {
+   window.alert("Please select atleast one type of character for generating password");
+ }
+  var n = charset.length;
+      output = "";
+  for (var i = 0; i < length; ++i) {
+      output += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return output;
+}
+var length = window.prompt("Enter the length of password.");
+var password = generatePassword(length);
+console.log(password);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
